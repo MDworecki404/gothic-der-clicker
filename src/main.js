4d94583df00a7ec172043b67d5da5beddd689a18
MDworecki404 <marek.dworecki01@gmail.com>
2024-12-22 12:32:50 +0100

    New features

--- src/main.js
+++ src/main.js
@@ -7,6 +7,10 @@ import SheepUpgrade from './assets/SheepUpgrade.png'
 import oldcampmusic from './assets/GothicOldCamp.mp3'
 import SaveGame from './assets/SaveGame.png'
 import LoadGame from './assets/LoadGame.png'
+import strengthPanel from './assets/Strength.png'
+import dexterityPanel from './assets/Dexterity.png'
+import RyzCollector from './assets/RyzCollectorPanel.png'
+import Slavery from './assets/SubjectSlavery.png'
 
 const config = {
   type: Phaser.AUTO,
@@ -28,6 +32,10 @@ function preload ()
   this.load.image('SheepUpgrade', SheepUpgrade)
   this.load.image('SaveGame', SaveGame)
   this.load.image('LoadGame', LoadGame)
+  this.load.image('StrengthPanel', strengthPanel)
+  this.load.image('DexterityPanel', dexterityPanel)
+  this.load.image('RyzCollectorPanel', RyzCollector)
+  this.load.image('SlaveryPanel', Slavery)
 
   this.load.audio('OldCampMusic', oldcampmusic)
 }
@@ -49,6 +57,13 @@ function create ()
 
   this.add.image(0,0, 'background').setOrigin(0,0)
 
+  let levelText = this.add.text(16,53, `Poziom: ${playerStats.stats.level}`, {fill: '#000', fontSize: '32px', fontFamily: 'Times New Roman'})
+  let expText = this.add.text(207,53, `Exp: ${playerStats.stats.expAct}/${playerStats.stats.expNeed}`, {fill: '#000', fontSize: '32px', fontFamily: 'Times New Roman'})
+  let PNText = this.add.text(16,90, `PN: ${playerStats.stats.PN}`, {fill: '#000', fontSize: '32px', fontFamily: 'Times New Roman'})
+  let STRText = this.add.text(16,124, `Siła: ${playerStats.stats.str}`, {fill: '#000', fontSize: '32px', fontFamily: 'Times New Roman'})
+  let DEXText = this.add.text(16,161, `Zręczność: ${playerStats.stats.dex}`, {fill: '#000', fontSize: '32px', fontFamily: 'Times New Roman'})
+
+  //Zapis i wczytanie gry//
   const saveButton = this.add.image(13, 706, 'SaveGame').setOrigin(0,0)
   saveButton.setInteractive({cursor: 'pointer'})
   saveButton.on('pointerdown', () => {
@@ -78,6 +93,11 @@ function create ()
           // Zaktualizuj wyświetlane wartości
           zlotoText.setText(`Złoto: ${playerStats.zloto.zloto}`)
           sheepText.setText(`Ilość: ${playerStats.upgrades.sheepAmount}`)
+          STRText.setText(`Siła: ${playerStats.stats.str}`)
+          DEXText.setText(`Zręczność: ${playerStats.stats.dex}`)
+          RyzCollectorText.setText(`Ilość: ${playerStats.upgrades.ryzCollectorAmount}`)
+          SlaveryText.setText(`Ilość: ${playerStats.upgrades.slaversAmount}`)
+          
         } catch (err) {
           console.error("Błąd podczas wczytywania pliku:", err)
           alert("Nieprawidłowy format pliku JSON! Edytuj plik za pomocą notatnika i usuń z jego zawartości fragment [object Object]")
@@ -90,19 +110,77 @@ function create ()
   LoadButton.on('pointerdown', () => {
     fileInput.click() // Wywołaj kliknięcie na ukrytym elemencie
   })
+  //Zapis i wczytanie gry
 
+  //Głowa do klikania//
   const head = this.add.image(1024/2,768/2, 'head1').setScale(0.5)
   zlotoText = this.add.text(16, 16, `Złoto: ${playerStats.zloto.zloto}`, {fontSize: '32px',fontFamily: 'Times New Roman', fill: '#000'})
   head.setInteractive({cursor: 'pointer'})
   head.on('pointerdown', () => {
     playerStats.zloto.zloto+=playerStats.zloto.zlotoAchiever
     zlotoText.setText(`Złoto: ${playerStats.zloto.zloto}`)
+    playerStats.stats.expAct+=1
+    expText.setText(`Exp: ${playerStats.stats.expAct}/${playerStats.stats.expNeed}`)
+    
   })
+  //Głowa do klikania//
+  //Odświeżanie przychodu i expa//
   setInterval(()=>{
     playerStats.zloto.zloto+=playerStats.zloto.zlotoPassive
     zlotoText.setText(`Złoto: ${playerStats.zloto.zloto}`)
-  },1000)
+    playerStats.stats.expAct+=playerStats.expPassive
+    expText.setText(`Exp: ${playerStats.stats.expAct}/${playerStats.stats.expNeed}`)
 
+    if(playerStats.stats.expAct >= playerStats.stats.expNeed){
+      playerStats.stats.level+=1
+      playerStats.stats.expAct=playerStats.stats.expAct-playerStats.stats.expNeed
+      playerStats.stats.expNeed+=500
+      playerStats.stats.PN+=1
+      levelText.setText(`Poziom: ${playerStats.stats.level}`)
+      expText.setText(`Exp: ${playerStats.stats.expAct}/${playerStats.stats.expNeed}`)
+      PNText.setText(`PN: ${playerStats.stats.PN}`)
+    }
+
+    //Console.log'i//
+      //console.log(playerStats.incomeTime)
+    //Console.log'i//
+  },playerStats.incomeTime)
+  //Odświeżanie przychodu i expa//
+
+
+  //Siła//
+  const Strength = this.add.image(16, 274, 'StrengthPanel').setOrigin(0,0)
+  Strength.setInteractive({cursor: 'pointer'})
+  Strength.on('pointerdown', () => {
+    if(playerStats.stats.PN>=1){
+      playerStats.stats.str+=1
+      playerStats.zloto.zlotoAchiever+=1
+      playerStats.stats.PN-=1
+      PNText.setText(`PN: ${playerStats.stats.PN}`)
+      STRText.setText(`Siła: ${playerStats.stats.str}`)
+    } else if (playerStats.stats.PN<1) {
+      alert('Brak ci doświadczenia!!')
+    }
+  })
+  //Siła//
+
+  //Zręczność//
+  const Dexterity = this.add.image(16, 390, 'DexterityPanel').setOrigin(0,0)
+  Dexterity.setInteractive({cursor: 'pointer'})
+  Dexterity.on('pointerdown', () => {
+    if(playerStats.stats.PN>=1){
+      playerStats.stats.dex+=1
+      playerStats.incomeTime=playerStats.incomeTime*0.95
+      playerStats.stats.PN-=1
+      PNText.setText(`PN: ${playerStats.stats.PN}`)
+      DEXText.setText(`Zręczność: ${playerStats.stats.dex}`)
+    } else if (playerStats.stats.PN<1) {
+      alert('Brak ci doświadczenia!!')
+    }
+  })
+  //Zręczność//
+
+  //Panel owcy//
   const SheepUpgradePanel = this.add.image(720, 44, 'SheepUpgrade').setOrigin(0,0)
   SheepUpgradePanel.setInteractive({cursor: 'pointer'})
   sheepText = this.add.text(728, 111, `Ilość: ${playerStats.upgrades.sheepAmount}`, {fontSize: '24px', fontFamily: 'Times New Roman', fill: '#fff'}).setOrigin(0,0)
@@ -113,13 +191,53 @@ function create ()
       zlotoText.setText(`Złoto: ${playerStats.zloto.zloto}`)
       sheepText.setText(`Ilość: ${playerStats.upgrades.sheepAmount}`)
       playerStats.zloto.zlotoPassive+=1
+      playerStats.expPassive+=1
 
     } else if (playerStats.zloto.zloto < 100) {
       alert('Biedaku, nawet złota nie masz!')
     }
-    
-    
   })
+  //Panel owcy//
+  //Panel Ryzu//
+  const RyzCollectorPanel = this.add.image(720,160,'RyzCollectorPanel').setOrigin(0,0)
+  let RyzCollectorText = this.add.text(728, 226, `Ilość: ${playerStats.upgrades.ryzCollectorAmount}`, {fontSize: '24px', fontFamily: 'Times New Roman', fill: '#fff'}).setOrigin(0,0)
+  let RyzCost = 1000
+  RyzCollectorPanel.setInteractive({cursor: 'pointer'})
+
+  RyzCollectorPanel.on('pointerdown', () => {
+    if(playerStats.zloto.zloto>=1000){
+        playerStats.upgrades.ryzCollectorAmount+=1
+        playerStats.zloto.zloto = playerStats.zloto.zloto-RyzCost
+        zlotoText.setText(`Złoto: ${playerStats.zloto.zloto}`)
+        RyzCollectorText.setText(`Ilość: ${playerStats.upgrades.ryzCollectorAmount}`)
+        playerStats.zloto.zlotoPassive+=5
+        playerStats.expPassive+=5
+
+      } else if (playerStats.zloto.zloto < 1000) {
+        alert('Biedaku, nawet złota nie masz!')
+      }
+  })
+  //Panel Ryzu//
+  //Dzisiejszy temat - niewolnictwo//
+  const SlaveryPanel = this.add.image(720,276, 'SlaveryPanel').setOrigin(0,0)
+  let SlaveryText = this.add.text(728, 342, `Ilość: ${playerStats.upgrades.slaversAmount}`, {fontSize: '24px', fontFamily: 'Times New Roman', fill: '#fff'}).setOrigin(0,0)
+  let slaverCost = 1500
+  SlaveryPanel.setInteractive({cursor: 'pointer'})
+
+  SlaveryPanel.on('pointerdown', ()=>{
+    if(playerStats.zloto.zloto>=1500){
+      playerStats.upgrades.slaversAmount+=1
+      playerStats.zloto.zloto = playerStats.zloto.zloto-slaverCost
+      zlotoText.setText(`Złoto: ${playerStats.zloto.zloto}`)
+      SlaveryText.setText(`Ilość: ${playerStats.upgrades.slaversAmount}`)
+      playerStats.zloto.zlotoPassive+=7.5
+      playerStats.expPassive+=7.5
+
+    } else if (playerStats.zloto.zloto < 1500) {
+      alert('Biedaku, nawet złota nie masz!')
+    }
+  })
+  //Dzisiejszy temat - niewolnictwo//
   
 }
 
--- src/player.js
+++ src/player.js
@@ -1,12 +1,24 @@
 const playerStats = {
+    stats: {
+        level: 0,
+        expAct: 0,
+        expNeed: 500,
+        PN: 0,
+        str: 0,
+        dex: 0
+    },
     upgrades: {
-        sheepAmount: 0
+        sheepAmount: 0,
+        ryzCollectorAmount: 0,
+        slaversAmount:0,
     },
     zloto: {
-        zloto: 0,
+        zloto: 99999, //Poźniej trzeba zmienić na 0
         zlotoAchiever: 1,
         zlotoPassive: 0,
-    }
+    },
+    expPassive: 0,
+    incomeTime: 1000,
 }
 
 export default playerStats
